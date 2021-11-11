e5f55dc7d22fdfd73adbe558a40858e915bbdcb4
Timothy Russo <russo.t@cisalfasport.it>
2021-11-11 11:52:34 +0100

    feat: DBconnection and morgan setup

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express');  // Creating basic Express server
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // Load the config file
 dotenv.config({ path: './config/config.env' });  // Where we put our global variables
 
+// Connect the DB
+connectDB();
+
 // Initialize the app
 const app = express();
 
-const PORT = process.env.PORT
+// Run morgan in development
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
-app.listen()
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
 
