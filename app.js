c08ca65b80a5296934ac78697ac7ca0da1726648
Timothy Russo <russo.t@cisalfasport.it>
2021-11-11 17:51:23 +0100

    feat: google oauth and password configuration

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+const path = require('path');
 const express = require('express');  // Creating basic Express server
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
 const connectDB = require('./config/db');
 
 // Load the config file
 dotenv.config({ path: './config/config.env' });  // Where we put our global variables
+
+// Passport config
+require('./config/passport')(passport);
 
 // Connect the DB
 connectDB();
@@ -12,10 +19,34 @@ connectDB();
 // Initialize the app
 const app = express();
 
-// Run morgan in development
+// Logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+
+// Handlebars
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+// Sessions
+app.use(
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false,
+    })
+)
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', require('./routes/index'));
+
 
 const PORT = process.env.PORT || 3000
 
