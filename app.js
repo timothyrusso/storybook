7a5e48f58278fe9a205c0bf33d51286eb09fa429
Timothy Russo <russo.t@cisalfasport.it>
2021-11-15 16:45:50 +0100

    feat: login authentication functions

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express = require('express');  // Creating basic Express server
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const passport = require('passport');
 const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 const connectDB = require('./config/db');
 
 // Load the config file
@@ -34,6 +36,7 @@ app.use(
         secret: 'keyboard cat',
         resave: false,
         saveUninitialized: false,
+        store: new MongoStore({ mongooseConnection: mongoose.connection })
     })
 )
 
@@ -46,7 +49,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/', require('./routes/index'));
-
+app.use('/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 3000
 
--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,30 @@
 
 const express = require('express');
 const router = express.Router();
+const { ensureAuth, ensureGuest } = require('../middleware/auth');
+
+const Story = require('../models/Story');
+
 
 // @desc   Login/Landing page
 // @route  GET
-router.get('/', (req, res) => {
-    res.render('login', {
-        layout: 'login',
-    })
+router.get('/', ensureGuest, async (req, res) => {
+    try {
+        const stories = await Story.find({ user: req.user.id }).lean()
+        res.render('login', {
+            layout: 'login',
+        })
+    } catch (err) {
+        
+    }
 })
 
 // @desc   Dashboard
 // @route  GET /dashboard
-router.get('/dashboard', (req, res) => {
-    res.render('dashboard')
+router.get('/dashboard', ensureAuth, (req, res) => {
+    res.render('dashboard', {
+        name: req.user.firstName,
+    })
 })
 
 module.exports = router;
