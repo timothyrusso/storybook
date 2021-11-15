730ed2c58c59ba84d82358fde6b68e17df4e8ebf
Timothy Russo <russo.t@cisalfasport.it>
2021-11-15 17:15:30 +0100

    feat: error pages and add button

--- routes/index.js
+++ routes/index.js
@@ -9,23 +9,25 @@ const Story = require('../models/Story');
 
 // @desc   Login/Landing page
 // @route  GET
-router.get('/', ensureGuest, async (req, res) => {
-    try {
-        const stories = await Story.find({ user: req.user.id }).lean()
-        res.render('login', {
-            layout: 'login',
-        })
-    } catch (err) {
-        
-    }
+router.get('/', ensureGuest, (req, res) => {
+    res.render('login', {
+        layout: 'login',
+    })
 })
 
 // @desc   Dashboard
 // @route  GET /dashboard
-router.get('/dashboard', ensureAuth, (req, res) => {
-    res.render('dashboard', {
-        name: req.user.firstName,
-    })
+router.get('/dashboard', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({ user: req.user.id }).lean()
+        res.render('dashboard', {
+            name: req.user.firstName,
+            stories
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
+    }
 })
 
 module.exports = router;
